1892bb9f34943b9fb201005644ffd921dfa15460
93c20-04 <student@93c20-04>
2019-03-06 17:59:33 +0800

    sys.path.append

--- FreeEnergySampling/annSampling/pyLMP/type_conversion/pylmp.py
+++ FreeEnergySampling/annSampling/pyLMP/type_conversion/pylmp.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 from lammps import lammps
 import numpy as np
-from ctypes import c_double
+import time
+import sys
+import os
+
+curdir = os.getcwd()
+sys.path.append(curdir)
+from train import test
 
 def post_force_callback(lammps_ptr, vflag):
 	lmp = lammps(ptr=lammps_ptr)
 	natoms = lmp.get_natoms()
 	f = lmp.extract_atom("f", 3) 
-	df = 500                      
 	F = np.ctypeslib.as_array(f.contents,shape=(natoms,3)) # c_double -> np.ndarray
+	test()	
+	df = 50
 	for i in range(natoms):
 		F[i][0] += df              
 		
-	
-
-
 # lammps.LP_LP_c_double --> np.ndarray by np.ctypeslib.as_array(obj=f.contents,shape=(natoms,3))
 # numpy ndarray shares the same memory blocks with ctypes pointer
 
---
+++ FreeEnergySampling/annSampling/pyLMP/type_conversion/train.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+
+def test():
+	print("here comes tensorflow")
+
