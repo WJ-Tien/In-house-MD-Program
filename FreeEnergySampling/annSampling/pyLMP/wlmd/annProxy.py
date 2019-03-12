d22d7cfc288c527bf2a1c9b2d12f639973c91251
93c20-04 <student@93c20-04>
2019-03-12 16:50:58 +0800

    ready to go

--- FreeEnergySampling/annSampling/pyLMP/wlmd/annProxy.py
+++ FreeEnergySampling/annSampling/pyLMP/wlmd/annProxy.py
@@ -5,7 +5,7 @@ import sys
 import os
 
 curdir = os.getcwd()
-sys.path.append(curdir) #TODO add path
+sys.path.append(curdir) 
 from annCore import trainingANN
 
 def post_force_callback(lammps_ptr, vflag):
@@ -17,16 +17,16 @@ def post_force_callback(lammps_ptr, vflag):
 	Ehigh       = -4.9
 	Elow        = -6.2
 	binsize     = 2.0
-	nbin        = int(np.ceil((Ehigh-Elow) * natoms / binsize)) + 1
+	nbin        = int(np.ceil((Ehigh-Elow) * natoms / binsize)) + 1 # binNum + 1
 	energyRange = np.linspace(-6.2, -4.9, nbin) 
 
-	T = lmp.extract_fix("ST", 2, 1) # 221 222 200 works
-	T = np.ctypeslib.as_array(T, shape=(nbin,))  #binNum + 1
-	T[-1] = T[0] #padding  rightMostBin 
+	T           = lmp.extract_fix("ST", 2, 1) # 221 222 200 works
+	T           = np.ctypeslib.as_array(T, shape=(nbin,)) 
+	T[-1]       = T[0] #padding the rightMostBin 
 
 	# training ~~
-	output = trainingANN("loss.dat", "hyperparam.dat", 1, nbin) # binNum + 1; therefore 72 + 1 
-	if step < 1000000:
+	output = trainingANN("loss.dat", "hyperparam.dat", 1, nbin) 
+	if step < 10000000:
 		T = output.training(energyRange, T, 0.025, 0.00025, 2500, 100)
 	else:
 		T = output.training(energyRange, T, 0.025, 0.00025, 7500, 100)
