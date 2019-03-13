cbd78c1431f9da391fcbaba549f5d19cfc9b77a5
93c20-04 <student@93c20-04>
2019-03-13 10:09:44 +0800

    upate annProxy

--- FreeEnergySampling/annSampling/pyLMP/wlmd/annProxy.py
+++ FreeEnergySampling/annSampling/pyLMP/wlmd/annProxy.py
@@ -20,13 +20,13 @@ def post_force_callback(lammps_ptr, vflag):
 	nbin        = int(np.ceil((Ehigh-Elow) * natoms / binsize)) + 1 # binNum + 1
 	energyRange = np.linspace(-6.2, -4.9, nbin) 
 
-	T           = lmp.extract_fix("ST", 2, 1) # 221 222 200 works
+	T           = lmp.extract_fix("ST", 2, 1) 
 	T           = np.ctypeslib.as_array(T, shape=(nbin,)) 
 	T[-1]       = T[0] #padding the rightMostBin 
 
 	# training ~~
 	output = trainingANN("loss.dat", "hyperparam.dat", 1, nbin) 
 	if step < 10000000:
-		T = output.training(energyRange, T, 0.025, 0.00025, 2500, 100)
+		T = output.training(energyRange, T, 0.075, 0.00025, 2500, 100)
 	else:
-		T = output.training(energyRange, T, 0.025, 0.00025, 7500, 100)
+		T = output.training(energyRange, T, 0.075, 0.00000, 7500, 100)
