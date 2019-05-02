a811f1c0363f30d0799214b68189d6b7c4ea6db7
93c20-04 <student@93c20-04>
2019-05-03 00:20:16 +0800

    it works

--- FreeEnergySampling/annSampling/new_MD_engine/integrator/integrator.py
+++ FreeEnergySampling/annSampling/new_MD_engine/integrator/integrator.py
@@ -4,20 +4,20 @@ from render import rendering
 from customMathFunc import paddingRightMostBin
 import numpy as np
 
-def integrator(ndims, cv ,force, half_boxboundary, outputfile):
+def integrator(ndims, cv, force, half_boxboundary, outputfile):
 
-  if ndims == 1:
+  if ndims == 1: # OK
 
     accIntg  = 0
     FE       = []
-    interval = abs(cv[0] - cv[1])
-    factor   = (interval * 0.5) ** ndims
+    intg_interval = abs(cv[0] - cv[1])
+    factor   = (intg_interval * 0.5) ** ndims
 
     for i in range(len(force) - 1):
-      accIntg -= (force[i] + force[i+1]) 
+      accIntg -= (force[i] + force[i+1])  # freeE = -Sf(x)dx
       FE.append(accIntg * factor)
-    
-    FE = paddingRightMostBin(FE)
+
+    FE.append(FE[0]) 
 
     with open(outputfile, "w") as fout:
       for i, j in zip(cv, FE):
@@ -25,29 +25,31 @@ def integrator(ndims, cv ,force, half_boxboundary, outputfile):
 
   #--------------------------------------------------#
 
-  if ndims == 2:
+  if ndims == 2: # Issues
 
-    interval = abs(cv[1][0][0] - cv[1][0][1])
-    factor   = (interval * 0.5) ** ndims 
+    intg_interval = abs(cv[1][0][0] - cv[1][0][1])
+    factor   = (intg_interval * 0.5) 
     FE_X     = 0
     FE_Y     = 0
-    acc_at_x = np.zeros((force.shape[1], force.shape[2]))
+    acc_at_x = np.zeros((force.shape[1]))
     acc_at_y = np.zeros((force.shape[1], force.shape[2]))
     FE       = np.zeros((force.shape[1], force.shape[2]))
 
     for i in range(force.shape[1] - 1):
+      FE_X = (force[0][i][0] + force[0][i+1][0])
+      acc_at_x[i] -= FE_X 
       for j in range(force.shape[2]- 1):
-        FE_X = (force[0][i][j] + force[0][i + 1][j])
-        acc_at_x[i][j] -= FE_X 
         FE_Y -= (force[1][i][j] + force[1][i][j+1])
         acc_at_y[i][j] = FE_Y
       FE_Y = 0
 
+    acc_at_x = np.append(acc_at_x, acc_at_x[-1])
+
     for i in range(force.shape[1]):
       for j in range(force.shape[2]):
-        FE[i][j] = (acc_at_x[i][j] + acc_at_y[i][j]) * factor
+        FE[i][j] = (acc_at_x[i] + acc_at_y[i][j]) * factor
 
-    FE += 30
+    FE += 29.5 
     FE = paddingRightMostBin(FE)
 
     with open(outputfile, "w") as fout:
@@ -58,6 +60,8 @@ def integrator(ndims, cv ,force, half_boxboundary, outputfile):
     s = rendering(ndims, half_boxboundary, force.shape[1] - 1)
     s.render(FE, name="FreeE_2D")
     
+
+
 if __name__ == "__main__":
 
   ndims = int(argv[1])
@@ -65,18 +69,22 @@ if __name__ == "__main__":
   if ndims == 1:
     cv    = []
     force = []
-    with open(argv[2], "r") as fin:
+    half_boxboundary = np.pi
+    with open("force_1D.dat", "r") as fin:
       for line in fin:
         line = line.split() 
         cv.append(float(line[0]))
         force.append(float(line[1]))
 
+
   if ndims == 2:
-    cv    = np.zeros((2, 41, 41))
-    force = np.zeros((2, 41, 41))
+    dsz   = 41 
+    cv    = np.zeros((ndims, dsz, dsz))
+    force = np.zeros((ndims, dsz, dsz))
     i     = 0
     j     = 0
-    with open(argv[2], "r") as fin:
+    half_boxboundary = 2
+    with open("force_2D.dat", "r") as fin:
       for line in fin:
         line = line.split()
         cv[0][i][j] = float(line[0])
@@ -84,9 +92,9 @@ if __name__ == "__main__":
         force[0][i][j] = float(line[2])
         force[1][i][j] = float(line[3])
         j += 1
-        if j == 41:
+        if j == dsz:
           i += 1
           j = 0
 
-  #integrator(ndims, cv, force, np.pi,  "FreeE_" + str(ndims) + "D.dat")
-  integrator(ndims, cv, force, 2,  "FreeE_" + str(ndims) + "D.dat")
+
+  integrator(ndims, cv, force, half_boxboundary,  "FreeE_" + str(ndims) + "D.dat")
